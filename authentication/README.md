8ada6af6efaea22fc1ebd25448cba09e6a80c8ca
Amit Gupta <amitgupta.gwl@gmail.com>
2018-10-06 13:38:40 +0530

    app setup

---
+++ authentication/README.md
@@ -0,0 +1,10 @@
+This is the sample application build using muneem web framework. It allows user to authenticate the application through google sign-in. 
+
+This application explain following concepts
+
+* Basic usse of Muneem framework
+* Using [Anuvadak (अनुवादक)](https://github.com/node-muneem/anuvadak) plugin to read url encoded forms and sending files from the server
+* Using [Satr (सत्र)](https://github.com/node-muneem/satr) plugin to manage session
+* Signing with Google
+
+You'll have to register with Google first to take client id. And replace it everywhere in the application.
---
+++ authentication/server.js
@@ -0,0 +1,103 @@
+var Muneem = require("muneem");
+var Anuvadak = require("anuvadak");
+var path = require("path");
+const satr = require("satr");
+const Satr = new satr();
+
+const CLIENT_ID = "CLIENT_ID.apps.googleusercontent.com";
+var app = Muneem({
+    server : {
+        requestId : true,
+        port : 7788
+    }
+});
+
+app.use(Anuvadak.urlForm);
+app.use(Anuvadak.sendFiles, {
+    root : path.join(__dirname, "static" )
+});
+app.use(Satr.manager, {
+    secret : "keep it secret and complex that no one can guess",
+    cookie : {
+        secure : false
+    }
+});
+
+function authentication(asked, answer, giveMe){
+    const session = asked.getSession(); 
+    if( !session.authorized ){
+        asked.hasSession = false; 
+        answer.redirectTo("/login.html?sendTo=" + asked.path);
+    }
+}
+
+var googleSignInHandler = async function(asked, answer){
+    await asked.readUrlForm();
+    const userid = await verifyTokenId(asked.body.idtoken);
+    if(userid){
+        const session = asked.getSession(); 
+        session.authorized = true;
+        answer.write( "ok" );
+    }else{
+        answer.close(403)
+    }
+}
+
+function logoutHandler(asked, answer, giveMe){
+    var session = asked.getSession();
+    session.authorized = false;
+    session.end();
+    asked.hasSession = false;
+    answer.redirectTo( "/" );
+}
+
+var loadStatic = function(asked, answer){
+    answer.sendFile();
+}
+
+app.route([{
+    uri : "/logout",
+    to : logoutHandler,
+},{
+    uri : "/login/google",
+    when : "POST",
+    to : googleSignInHandler,
+},{
+    uri : "/login.html",
+    to : loadStatic,
+},{
+    uri : "/",
+    to : loadStatic,
+},{
+    uri : "/dashboard.html",
+    to : loadStatic,
+    after: authentication
+}]);
+
+app.start();
+
+
+const {OAuth2Client} = require('google-auth-library');
+
+const client = new OAuth2Client(CLIENT_ID);
+async function verifyTokenId(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    
+    if( payload['aud'] !== CLIENT_ID){
+        //raise a security alarm
+    }else{
+    const userid = payload['sub'];
+    return userid;
+    //check if the user is present in the DB
+    //if not save in DB
+    //then create a new session and send session id to the user. So that you need not to verify token id for every request
+    }
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+}
